de0d28c00505e95d0d5e97d63df1ed4c65239618
Jonas <soler.jonas10@gmail.com>
2026-02-21 11:54:19 -0300

    CurrencyDTO

--- Models/Currency.cs
+++ Models/Currency.cs
@@ -1,11 +1,15 @@
-﻿namespace Currency.API.Models
+﻿using Currency.API.Models.DTOs;
+
+namespace Currency.API.Models
 {
-    public class Currency
+    public class Currency : CurrencyDTO
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Code { get; set; }
-        public required decimal RateToBase { get; set; }
+
+        //propiedades heredadas de CurrencyDTO
+        //public required string Name { get; set; }
+        //public required string Code { get; set; }
+        //public required decimal RateToBase { get; set; }
         
     }
 }
---
+++ Models/DTOs/CurrencyDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Currency.API.Models.DTOs
+{
+    public class CurrencyDTO
+    {
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+        public required decimal RateToBase { get; set; }
+    }
+}
