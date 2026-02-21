b7bcad6ae280bf08bf4aec1e4c3bc5929f58063b
Jonas <soler.jonas10@gmail.com>
2026-02-21 11:57:27 -0300

    Creacion de AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Currency.API.Models;
+
+namespace Currency.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Address> Addresses { get; set; }
+        DbSet<Models.Currency> Currencies { get; set; }
+    }
+}
