8a38399d45f9f219436247cc3ea342954b2dd5e7
Jonas <soler.jonas10@gmail.com>
2026-02-21 17:53:52 -0300

    Creacion de UserValidator

---
+++ Validators/UserValidator.cs
@@ -0,0 +1,11 @@
+﻿using Currency.API.Application.Users.Commands;
+using FluentValidation;
+
+public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(x => x.userDTO.Name).NotEmpty();
+        RuleFor(x => x.userDTO.Email).EmailAddress().NotEmpty();
+    }
+}
