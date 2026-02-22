9e2a84424ce74823c6bacf7f00778eea27bbdc21
Jonas <soler.jonas10@gmail.com>
2026-02-22 10:01:30 -0300

    Mod de UserValidator

--- Validators/UserValidator.cs
+++ Validators/UserValidator.cs
@@ -3,21 +3,26 @@ using FluentValidation;
 using Currency.API.Data;
 using Microsoft.EntityFrameworkCore;
 
-public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+namespace Currency.API.Validators
 {
-    private readonly AppDbContext _db;
-
-    public CreateUserValidator(AppDbContext db)
+    public class CreateUserValidator : AbstractValidator<CreateUserCommand>
     {
-        _db = db;
+        private readonly AppDbContext _db;
 
-        RuleFor(x => x.userDTO.Name).NotEmpty();
+        public CreateUserValidator(AppDbContext db)
+        {
+            _db = db;
 
-        RuleFor(x => x.userDTO.Email)
-            .EmailAddress()
-            .NotEmpty()
-            .MustAsync(async (email, cancellation) =>
-                !await _db.Users.AnyAsync(u => u.Email == email, cancellation))
-            .WithMessage("El email ya existe.");
+            RuleFor(x => x.userDTO.Name).NotEmpty();
+
+            RuleFor(x => x.userDTO.Email)
+                .EmailAddress()
+                .NotEmpty()
+                .MustAsync(async (email, cancellation) =>
+                    !await _db.Users.AnyAsync(u => u.Email == email, cancellation))
+                .WithMessage("El email ya existe.");
+
+            RuleFor(x => x.userDTO.Password).NotEmpty();
+        }
     }
 }
