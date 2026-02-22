4401871b6e2a53986038cb98a6f32d66f646032b
Jonas <soler.jonas10@gmail.com>
2026-02-22 19:45:51 -0300

    Modelo y DTO para conversion

---
+++ Models/CurrencyConversion.cs
@@ -0,0 +1,10 @@
+﻿namespace Currency.API.Models
+{
+    public class CurrencyConversion
+    {
+        public required string fromCurrencyCode { get; set; }
+        public required string toCurrencyCode { get; set; }
+        public decimal amount { get; set; }
+        public decimal convertedAmount { get; set; }
+    }
+}
---
+++ Models/DTOs/CurrencyConversionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Currency.API.Models.DTOs
+{
+    public class CurrencyConversionDTO
+    {
+        public required string fromCurrencyCode { get; set; }
+        public required string toCurrencyCode { get; set; }
+        public decimal amount { get; set; }
+    }
+}
