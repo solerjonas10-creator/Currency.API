2a8f269267155a68ca592cbbfc81ce440c8e2437
Jonas <soler.jonas10@gmail.com>
2026-02-22 14:54:34 -0300

    Validator de AddressDTO

---
+++ Validators/AddressValidator.cs
@@ -0,0 +1,27 @@
+﻿using Currency.API.Application.Addresses.Commands;
+using Currency.API.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Currency.API.Validators
+{
+    public class CreateAddressValidator : AbstractValidator<CreateAddressCommand>
+    {
+        private readonly AppDbContext _db;
+        public CreateAddressValidator(AppDbContext db)
+        {
+            _db = db;
+            /* -- El requerimiento pide que devuelva 404, por lo que maneje esta validacion en el CreateAddressCommand.cs
+            RuleFor(x => x.UserId)
+            .MustAsync(async (id, cancellation) =>
+                await db.Users.AnyAsync(u => u.Id == id, cancellation))
+            .WithMessage("El usuario especificado no existe."); 
+            */
+
+            RuleFor(x => x.addressDTO.Street).NotEmpty();
+            RuleFor(x => x.addressDTO.City).NotEmpty();
+            RuleFor(x => x.addressDTO.Country).NotEmpty();
+            RuleFor(x => x.addressDTO.ZipCode).NotEmpty();
+        }
+    }
+}
