315506d37b05e3010bbf3136b727feca8047759c
Jonas <soler.jonas10@gmail.com>
2026-02-22 09:02:16 -0300

    Correccion UserValidator

--- Validators/UserValidator.cs
+++ Validators/UserValidator.cs
@@ -1,11 +1,23 @@
 ﻿using Currency.API.Application.Users.Commands;
 using FluentValidation;
+using Currency.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 public class CreateUserValidator : AbstractValidator<CreateUserCommand>
 {
-    public CreateUserValidator()
+    private readonly AppDbContext _db;
+
+    public CreateUserValidator(AppDbContext db)
     {
+        _db = db;
+
         RuleFor(x => x.userDTO.Name).NotEmpty();
-        RuleFor(x => x.userDTO.Email).EmailAddress().NotEmpty();
+
+        RuleFor(x => x.userDTO.Email)
+            .EmailAddress()
+            .NotEmpty()
+            .MustAsync(async (email, cancellation) =>
+                !await _db.Users.AnyAsync(u => u.Email == email, cancellation))
+            .WithMessage("El email ya existe.");
     }
 }
