31d2dc0d5d9a4490e1064f1dd86f837bd4cc5f70
Jonas <soler.jonas10@gmail.com>
2026-02-22 19:46:36 -0300

    Validator del DTO coversion

---
+++ Validators/ConversionValidator.cs
@@ -0,0 +1,34 @@
+﻿using Currency.API.Application.Currency.Conversion;
+using Currency.API.Application.Users.Commands;
+using Currency.API.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Currency.API.Validators
+{
+    public class CalculateConversionValidator : AbstractValidator<CurrencyConversionCalculate>
+    {
+        private readonly AppDbContext _db;
+
+        public CalculateConversionValidator(AppDbContext db)
+        {
+            _db = db;
+
+            RuleFor(x => x.conversionDTO.fromCurrencyCode)
+                .NotEmpty()
+                .MustAsync(async (code, cancellation) =>
+                    await _db.Currencies.AnyAsync(c => c.Code == code, cancellation)
+                ).WithMessage("El codigo de fromCurrencyCode no existe.");
+
+            RuleFor(x => x.conversionDTO.fromCurrencyCode)
+                .NotEmpty()
+                .MustAsync(async (code, cancellation) =>
+                    await _db.Currencies.AnyAsync(c => c.Code == code, cancellation)
+                ).WithMessage("El codigo de toCurrencyCode no existe.");
+
+            RuleFor(x => x.conversionDTO.amount)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
