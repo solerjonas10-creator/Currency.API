acab96e6c0957e6b199b8918a9339d5e362ef2a5
Jonas <soler.jonas10@gmail.com>
2026-02-22 17:25:28 -0300

    Migracion Delete en Cascada Addresses cuando se elimina Usuario

---
+++ Migrations/20260222202422_UpdateDeleteCascadeAddresses.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Currency.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDeleteCascadeAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Users_UserId",
+                table: "Addresses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Users_UserId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses");
+        }
+    }
+}
