41257a13f079e890adfce869cfa390cc257c881e
Jonas <soler.jonas10@gmail.com>
2026-02-22 22:29:05 -0300

    Lastmigration

---
+++ Migrations/20260223012828_LastMigration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Currency.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Currencies_Code",
+                table: "Currencies",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Currencies_Code",
+                table: "Currencies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
